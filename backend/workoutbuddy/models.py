bce501c958c3bf59f0d5f46de13fdbb474b0b249
qrlandry <qrlandry@gmail.com>
2023-04-07 12:40:42 -0400

    adding register functionality

--- backend/workoutbuddy/models.py
+++ backend/workoutbuddy/models.py
@@ -2,7 +2,8 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager, Group, Permission
 
 class User(AbstractUser):
-    name: models.CharField(max_length=150)
+    first_name: models.CharField(max_length=150)
+    last_name: models.CharField(max_length=150)
     email = models.EmailField(max_length=150, unique=True)
     password = models.CharField(max_length=150)
     username = None
@@ -31,3 +32,6 @@ class User(AbstractUser):
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['name']
+    
+    def __str__(self):
+        return self.name
--- backend/workoutbuddy/views.py
+++ backend/workoutbuddy/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .serializers import UserSerializer
+from .models import User
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+class RegisterView(APIView):
+    queryset = User.objects.all()
+    permission_classes = [AllowAny]
+    def post(self, req):
+        serializer = UserSerializer(data=req.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
