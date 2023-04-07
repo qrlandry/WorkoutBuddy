b478f98ed304baa0e659d8e206473225ab6f7062
qrlandry <qrlandry@gmail.com>
2023-04-07 12:19:44 -0400

    added user model

--- backend/workoutbuddy/models.py
+++ backend/workoutbuddy/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager, Group, Permission
 
-# Create your models here.
+class User(AbstractUser):
+    name: models.CharField(max_length=150)
+    email = models.EmailField(max_length=150, unique=True)
+    password = models.CharField(max_length=150)
+    username = None
+    weight = models.IntegerField()
+
+    groups = models.ManyToManyField(
+        Group,
+        verbose_name=('groups'),
+        blank=True,
+        related_name='custom_user_set',
+        help_text=(
+            'The groups this user belongs to. A user will get all permissions '
+            'granted to each of their groups.'
+        ),
+        related_query_name='user',
+    )
+    
+    user_permissions = models.ManyToManyField(
+        Permission,
+        verbose_name=('user permissions'),
+        blank=True,
+        related_name='custom_user_set',
+        help_text=('Specific permissions for this user.'),
+        related_query_name='user',
+    )
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
