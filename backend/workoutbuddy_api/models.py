2e82ecddcddced5e8a646647fcf91c0c60bc4c4f
qrlandry <qrlandry@gmail.com>
2023-04-10 09:38:33 -0400

    user model and register view

--- backend/workoutbuddy_api/models.py
+++ backend/workoutbuddy_api/models.py
@@ -1,6 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 
+class UserManager(BaseUserManager):
+    def create_user(self, email, name, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, name, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, name, password, **extra_fields)
+
 class User(AbstractUser):
     name = models.CharField(max_length=150)
     email = models.EmailField(max_length=150, unique=True)
@@ -10,6 +25,7 @@ class User(AbstractUser):
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['name']
+    objects = UserManager()
 
     groups = models.ManyToManyField(
         'auth.Group',
--- backend/workoutbuddy_api/views.py
+++ backend/workoutbuddy_api/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .serializers import UserSerializer
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+class RegisterView(APIView):
+    def post(self, req):
+        serializer = UserSerializer(data=req.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
