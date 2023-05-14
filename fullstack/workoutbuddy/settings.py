7e58d6a3cf1dac668bec78fc07f8fee25cc41656
qrlandry <qrlandry@gmail.com>
2023-05-14 18:30:39 -0400

    dep

--- fullstack/workoutbuddy/settings.py
+++ fullstack/workoutbuddy/settings.py
@@ -146,11 +146,8 @@ ALLOWED_HOSTS = ["*"]
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-MEDIA_URL = 'media/'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- fullstack/workoutbuddy/urls.py
+++ fullstack/workoutbuddy/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.http import HttpResponse
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('workoutbuddy_api.urls')),
 ]
 
-urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
